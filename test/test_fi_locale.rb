4b95ddc2bf4d452aa6ea72d8100149579acdec2f
Joel Lehikoinen <joel.lehikoinen@netmedi.fi>
2016-01-27 19:29:05 +0200

    renamed locale fi-FI -> fi

--- test/test_fi_locale.rb
+++ test/test_fi_locale.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + '/test_helper.rb')
 
 class TesetEnLocale < Test::Unit::TestCase
   def setup
-    Faker::Config.locale = 'fi-FI'
+    Faker::Config.locale = 'fi'
   end
 
   def teardown
